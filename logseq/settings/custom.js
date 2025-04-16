1b945152727840956a496613e8725a1414783065
Luke McCarthy <thelukemccarthy@gmail.com>
2025-04-16 10:30:01 +1000

    Add code to custom.js file to change the accent colour and theme when the graph is changed. Very useful for knowing when you are using different graphs ir personal and work.

--- logseq/settings/custom.js
+++ logseq/settings/custom.js
@@ -17,3 +17,51 @@ bulletsObserver.observe(document.getElementById('app-container'), {
     childList: true,
     subtree: true
 });
+
+const changeThemeAndAccentColor = () => {
+    // Valid Colours -> 'none', 'logseq', 'tomato', 'red', 'crimson', 'pink', 'plumb', 'purple', 'violet', 'indigo', 'blue', 'cyan', 'teal', 'green', 'grass', 'orange'
+    let ACCENT_COLOR = 'logseq';
+    let THEME = 'dark'; // Valid Themes -> 'light', 'dark'
+
+    if(logseq.api.get_current_graph().name !== 'logseq'){ 
+        ACCENT_COLOR = 'red';
+        THEME = 'light'; 
+    }
+
+    console.log(`Setting theme to ${THEME} and accent color to ${ACCENT_COLOR} for graph ${logseq.api.get_current_graph().name}`);
+
+    const htmlTag = document.documentElement;
+    const bodyTag = document.body;
+
+    htmlTag.dataset.color = ACCENT_COLOR;
+    htmlTag.dataset.theme = THEME; 
+
+    const changeToLightTheme = () => {
+        htmlTag.classList.remove('dark');
+        bodyTag.classList.remove('dark-theme');
+
+        bodyTag.classList.add('white-theme');
+        bodyTag.classList.add('light-theme');
+    }
+    
+    const changeToDarkTheme = () => {
+        bodyTag.classList.remove('white-theme');
+        bodyTag.classList.remove('light-theme');
+    
+        bodyTag.classList.add('dark-theme');
+        htmlTag.classList.add('dark');
+    }
+
+    THEME === 'light' ? changeToLightTheme() : changeToDarkTheme();
+};
+
+const graphChangedObserver = new MutationObserver(() => {
+    changeThemeAndAccentColor();
+});
+
+const graphChangedObserverConfig = { childList: true, subtree: true, characterDataOldValue: true, attributes: false };
+const currentGraphElement = document.getElementById('repo-name');
+
+graphChangedObserver.observe(currentGraphElement, graphChangedObserverConfig);
+
+changeThemeAndAccentColor();
